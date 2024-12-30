6ae6753fc4d4b7d3a65f89cea9ae37610d8454de
Deepesh <deepesh@mail.upb.de>
2024-12-30 11:04:08 +0100

    Changes to RTA with test case passing

--- src/main/java/analysis/exercise2/RTAAlgorithm.java
+++ src/main/java/analysis/exercise2/RTAAlgorithm.java
@@ -3,7 +3,22 @@ package analysis.exercise2;
 import analysis.CallGraph;
 import analysis.exercise1.CHAAlgorithm;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import sootup.core.IdentifierFactory;
+import sootup.core.jimple.common.stmt.Stmt;
+import sootup.core.model.SootClass;
+import sootup.core.model.SootClassMember;
+import sootup.core.signatures.MethodSignature;
+import sootup.java.core.JavaSootClass;
 import sootup.java.core.views.JavaView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class RTAAlgorithm extends CHAAlgorithm {
 
@@ -19,6 +34,45 @@ public class RTAAlgorithm extends CHAAlgorithm {
     // To get your entry points we prepared getEntryPoints(view) in the superclass for you
 
     // TODO: implement
+    final IdentifierFactory identifierFactory = view.getIdentifierFactory();
+    for (MethodSignature methodSignature : getEntryPoints(view).collect(Collectors.toList())) {
+        buildRTA(methodSignature, cg, view, null);
+        MethodSignature original = identifierFactory.parseMethodSignature(methodSignature.toString());
 
+        getConnectingPoints(view, methodSignature)
+                .stream()
+                .filter(connectingMethods -> cg.edgesOutOf(original).stream()
+                        .anyMatch(connected -> connectingMethods.getDeclClassType() == connected.getDeclClassType()))
+                .forEach(connectingMethods -> buildRTA(connectingMethods, cg, view, original));
+    }
   }
+  protected void buildRTA(MethodSignature methodSignature, CallGraph cg, JavaView view, @Nullable MethodSignature original) {
+    final IdentifierFactory identifierFactory = view.getIdentifierFactory();
+    if (original != null) {
+        addNode(cg, methodSignature);
+        addEdge(cg, original, methodSignature);
+    }
+
+    //  System.out.println(view.getMethod(methodSignature).get().getBody());
+    if (!view.getMethod(methodSignature).isPresent() || !view.getMethod(methodSignature).get().hasBody()) return;
+
+    addNode(cg, methodSignature);
+    for (Stmt stmt : view.getMethod(methodSignature).get().getBody().getStmts()) {
+        System.out.println(stmt);
+        if (!stmt.containsInvokeExpr()) continue;
+        MethodSignature connectingMethod = stmt.getInvokeExpr().getMethodSignature();
+        addNode(cg, connectingMethod);
+        addEdge(cg, methodSignature, connectingMethod);
+
+        if (original != null) {
+            addEdge(cg, original, connectingMethod);
+        }
+
+        try {
+            buildRTA(connectingMethod, cg, view, methodSignature);
+        } catch (Exception ignored) {
+        }
+
+    }
+}
 }
